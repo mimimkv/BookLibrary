99241670574b643b5ebd6179196a7779b866b4bd
mimimkv <mvmark@abv.bg>
2022-05-04 08:47:50 +0300

    Implement Delete request in BookController

--- src/main/java/com/example/booklibrary/controller/BookController.java
+++ src/main/java/com/example/booklibrary/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.example.booklibrary.controller;
 
 import com.example.booklibrary.dto.BookDto;
+import com.example.booklibrary.exceptions.BookNotFoundException;
 import com.example.booklibrary.model.Book;
 import com.example.booklibrary.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/library") //-> localhost:8080/library
 public class BookController {
+    private static final String BOOK_NOT_FOUND_MESSAGE_TEMPLATE = "Book with isbn %d not found";
+
     private final BookRepository bookRepository;
 
     @Autowired
@@ -35,5 +40,14 @@ public class BookController {
         book.setCategory(bookDto.getCategory());
 
         return bookRepository.save(book);
+    }
+
+    @DeleteMapping("/books/{isbn}")
+    public void delete(@PathVariable Long isbn) throws BookNotFoundException {
+        if (!bookRepository.existsById(isbn)) {
+            throw new BookNotFoundException(String.format(BOOK_NOT_FOUND_MESSAGE_TEMPLATE, isbn));
+        }
+
+        bookRepository.deleteById(isbn);
     }
 }

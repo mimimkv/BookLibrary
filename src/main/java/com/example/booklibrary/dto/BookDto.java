2234c4d69130cd5138b17f7484ec072fb8826c03
mimimkv <mvmark@abv.bg>
2022-05-07 09:30:14 +0300

    Add and Implement BookService

--- src/main/java/com/example/booklibrary/dto/BookDto.java
+++ src/main/java/com/example/booklibrary/dto/BookDto.java
@@ -1,5 +1,6 @@
 package com.example.booklibrary.dto;
 
+import com.example.booklibrary.model.Book;
 import com.example.booklibrary.model.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,4 +11,13 @@ public class BookDto {
     private String title;
     private String author;
     private Category category;
+
+    public static Book mapToBook(BookDto bookDto) {
+        Book book = new Book();
+        book.setTitle(bookDto.getTitle());
+        book.setAuthor(bookDto.getAuthor());
+        book.setCategory(bookDto.getCategory());
+
+        return book;
+    }
 }
